ed4c47dd9fabafafc7303c102008661e5ea4f3f5
Nancy Mazariegos <nancymazariegos03@gmail.com>
2023-09-26 21:14:03 -0600

    Ingreso e LogIn

--- app/src/main/java/com/example/lab4_ppm/Ingreso.kt
+++ app/src/main/java/com/example/lab4_ppm/Ingreso.kt
@@ -3,8 +3,9 @@ package com.example.lab4_ppm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Ingreso : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ingreso)
     }
 }
--- app/src/main/java/com/example/lab4_ppm/InicioSesion.kt
+++ app/src/main/java/com/example/lab4_ppm/InicioSesion.kt
@@ -3,9 +3,9 @@ package com.example.lab4_ppm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class CrearUsuario : AppCompatActivity() {
+class InicioSesion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_crear_usuario)
+        setContentView(R.layout.activity_inicio_sesion)
     }
 }
